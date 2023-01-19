9934d2258b0e1304d9e0fe86f0cde77dbb7df600
Eduardo Oliveira <edumottaoliveira@gmail.com>
2023-01-19 15:30:31 -0800

    Create Employees model

---
+++ EmployeeDirectory/Employees/Models/Employees.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct Employees: Decodable {
+    let employees: [Employee]
+}
+
+struct Employee: Decodable {
+    enum `Type`: String, Decodable {
+        case fullTime = "FULL_TIME"
+        case partTime = "PART_TIME"
+        case contractor = "CONTRACTOR"
+    }
+    
+    let uuid: String
+    let fullName: String
+    let phoneNumber: String
+    let emailAddress: String
+    let biography: String
+    let photoUrlSmall: String
+    let photoUrlLarge: String
+    let team: String
+    let employeeType: `Type`
+}
